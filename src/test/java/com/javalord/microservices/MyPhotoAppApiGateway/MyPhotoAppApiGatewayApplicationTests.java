94eeec93615ae3304fbf64b10489c27d54ff60b5
Christopher-OOC <olojedechristopher24@gmail.com>
2025-03-10 00:35:22 +0100

    commit

---
+++ src/test/java/com/javalord/microservices/MyPhotoAppApiGateway/MyPhotoAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalord.microservices.MyPhotoAppApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotoAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
